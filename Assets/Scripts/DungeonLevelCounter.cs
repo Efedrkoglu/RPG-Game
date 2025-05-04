676ecee0d36ad177842fb78a64b1221418bee152
Efe Durukoglu <efedurukoglu@gmail.com>
2025-05-04 22:42:01 +0300

    Dungeon level

---
+++ Assets/Scripts/DungeonLevelCounter.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonLevelCounter : MonoBehaviour
+{
+    public void ExitDungeonButton() {
+        Debug.Log("Exit btn.");
+    }
+}
